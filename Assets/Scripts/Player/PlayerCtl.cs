f769aa456464dc34c89dfa30a73d0727c23958c9
Guiny-Time <1084926982@qq.com>
2023-03-22 20:29:55 +0800

    Refactoring player function

--- Assets/Scripts/Player/PlayerCtl.cs
+++ Assets/Scripts/Player/PlayerCtl.cs
@@ -10,13 +10,15 @@ public class PlayerCtl : MonoBehaviour
     public int magic;
     public LineRenderer lr;
 
-    private GameObject pos;
+    
     private List<AStarNode> result = new List<AStarNode>();
     private List<AStarNode> temp_result = new List<AStarNode>();
     private GameObject highlightBlocks = null;
     private bool moveState = false; //false: stand; true: moving
     private int stepCount = 1;  // foot step
     private Vector2 temp;
+    private GameObject pos; // mouse choose obj
+    private bool inMagic;   // whether in magic state
 
     
     private void Awake()
@@ -28,42 +30,101 @@ public class PlayerCtl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        inMagic = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         ChooseBlock();
-        if(moveState)
+        if (pos != null)
         {
-            Moving();
+            result = AStarMgr.GetInstance().FindPath(new Vector2( Mathf.Round(transform.position.z), Mathf.Round(transform.position.x)),
+                new Vector2(pos.transform.position.z,pos.transform.position.x));
         }
-        if(Input.GetMouseButtonDown(0))
+        
+        if(moveState) { Moving(); }     // moving
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (result.Count != 0)
+            print("开始使用重力魔法");
+            inMagic = true;
+            GravityMagic();
+        }
+
+        if (inMagic)
+        {
+            if(Input.GetMouseButtonDown(0))
             {
-                temp_result = result;
-                stepCount = 0; 
-                moveState = true;
+                print("对该方块施展重力魔法：" + pos.name);
+                // gravity magic
+                inMagic = false;
             }
-        }   
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                print("退出施法状态");
+                inMagic = false;
+            }
+        }
+        else
+        {
+            DrawLine();
+            if(Input.GetMouseButtonDown(0))
+            {
+                if (result.Count != 0)
+                {
+                    temp_result = result;
+                    stepCount = 0; 
+                    moveState = true;
+                }
+            }
+        }
     }
 
     /// <summary>
-    /// 选择移动目标
+    /// 使用重力魔法
+    /// </summary>
+    void GravityMagic()
+    {
+        pos = InputMgr.GetInstance().GetCurrentMouse();
+        // clear highlight
+        if (highlightBlocks != null)
+        {
+            highlightBlocks.GetComponent<Outline>().enabled = false;
+        }
+        highlightBlocks = null;
+        lr.positionCount = 0;
+        
+        if (pos.GetComponent<Outline>())
+        {
+            if (highlightBlocks != null && pos.name != highlightBlocks.name)
+            {
+                highlightBlocks.GetComponent<Outline>().enabled = false;
+                highlightBlocks = pos;
+                highlightBlocks.GetComponent<Outline>().enabled = true;
+            }
+            else
+            {
+                highlightBlocks = pos;
+                highlightBlocks.GetComponent<Outline>().enabled = true;
+            }
+        }
+        
+    }
+
+    /// <summary>
+    /// 选择目标
     /// </summary>
     void ChooseBlock()
     {
         pos = InputMgr.GetInstance().GetCurrentMouse();
         try
         {
-            result = AStarMgr.GetInstance().FindPath(new Vector2( Mathf.Round(transform.position.z), Mathf.Round(transform.position.x)),new Vector2(pos.transform.position.z,pos.transform.position.x));
-            if (result == null)
+            if (result == null && !inMagic)
             {
                 highlightBlocks.GetComponent<Outline>().enabled = false;
                 highlightBlocks = null;
-                lr.SetVertexCount(0);
             }
             else
             {
@@ -78,18 +139,36 @@ public class PlayerCtl : MonoBehaviour
                     highlightBlocks = pos;
                     highlightBlocks.GetComponent<Outline>().enabled = true;
                 }
-
-                lr.positionCount = result.Count;
-                for (int i = 0; i < result.Count; i++)
-                {
-                    lr.SetPosition(i,new Vector3(result[i].y, 0, result[i].x));
-                }
             }
         }
         catch (Exception e)
         {
-            lr.SetVertexCount(0);
+            return;
         }
+    }
+
+    /// <summary>
+    /// 画线，用于展示自动寻路路线
+    /// </summary>
+    void DrawLine()
+    {
+        if (result == null)
+        {
+            lr.positionCount = 0;
+        }
+        try
+        {
+            lr.positionCount = result.Count;
+            for (int i = 0; i < result.Count; i++)
+            {
+                lr.SetPosition(i, new Vector3(result[i].y, 0, result[i].x));
+            }
+        }
+        catch (Exception e)
+        {
+            lr.positionCount = 0;
+        }
+        
     }
 
     void Moving()
